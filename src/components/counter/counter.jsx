9e16243ca5787643ff4ab23b2159d7f8dba5a9c8
Andy Albarracin <albarracin.andres@gmail.com>
2022-12-10 14:52:55 -0300

    Entrega Carrito

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -4,15 +4,19 @@ import CustomButtom from '../customButton/CustomButton'
 import { useState } from 'react'
 
 
-const Counter = () => { 
+const Counter = (stock, initial, onAdd) => { 
     
-    const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(initial)
 
 
     return (
     <div>
         <h2>{counter}</h2>
         <CustomButtom texto={"Sumar"} counter={counter} setCounter={setCounter}/>
+        <button>Sumar</button>
+        <button>Restar</button>
+        <button>Agregar al carrito</button>
+
     </div>
   )
 }
--- src/components/itemListContainer/ItemListContainer.js
+++ src/components/itemListContainer/ItemListContainer.js
@@ -1,29 +1,48 @@
 
-import {useState} from "react"
+import {useState, useEffect} from "react"
 //import CustomButtom from "../customButton/CustomButton"
 import "./ItemListContainer.css"
+import Counter from '../counter/counter'
 
 const ItemListContainer = ( { greeting } ) => {
 
     const [ isDark, setIsDark ] = useState( false )
+    const [ num , setNum ] = useState(0)
 
     const tooggleMode = () =>{
         
-        /* if(isDark === true){
-            setIsDark(false)
-        }else if(isDark === false){ 
-            setIsDark(true)
-        } */
-
         setIsDark  ( !isDark )
-
     }
+
     console.log(isDark)
+    
+    useEffect( ()=>{
+        console.log("Hola dentro del primer Efecto")
+    } )
+
+    useEffect( ()=>{
+        console.log("Hola desde el efecto con array de dependencia VACIO")
+    }, [] ) //arreglo de dependencia
+    useEffect( ()=>{
+        console.log("Hola desde el efecto con array de dependencia a la escucha de NUM")
+    }, [ num ] ) //arreglo de dependencia
+
+    const onAdd = ()=>{
+        console.log("Hola")
+    }
+
     return (
-        <div className={isDark ? "container-items-dark" : "container-items-light"}>
+    <div className={isDark ? "container-items-dark" : "container-items-light"}>
             <h2>{greeting}</h2>
             <button onClick={tooggleMode}>
-                {isDark ? "Cambiar a Modo Claro" : "Cambiar a Modo Oscuro"}</button>            
+                {isDark ? "Cambiar a Modo Claro" : "Cambiar a Modo Oscuro"}
+            </button>
+            <button onClick={()=> setNum(num + 1)}>
+                Sumar
+            </button>
+
+            <Counter stock={10} initial={1} onAdd={onAdd} /> 
+
         </div>
         )
 }
